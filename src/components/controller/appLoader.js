74164e0d3349b69d764c54fa95240c8acff06dd9
julia <yusiagerachka@gmail.com>
2022-12-09 00:00:22 +0300

    feat:add key to get news

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: 'e14ed9e9c42940b09bfe68dfc19f1fcd', // получите свой ключ https://newsapi.org/
         });
     }
 }
