745544702c53cd0451f178241c0afc68c8e3f235
julia <yusiagerachka@gmail.com>
2022-12-10 17:58:42 +0300

    feat: add eslint , webpack configuration for ts

---
+++ .eslintrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  root: true,
+  ignorePatterns: [ "**/config.js"],
+};
